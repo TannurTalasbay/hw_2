094ed9c734dde9c22e190634c4ab58bc44db9682
User <231583@astanait.edu.kz>
2024-01-25 23:37:19 +0600

    variables

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,10 @@
 public class Student {
 }
+public class Student extends Person {
+    private double gpa;
+    private static final double step = 36660.00;
+
+    public Student() {
+        super();
+    }
+}
