be2a18e0900c76bd12d717357575945537c95586
User <231583@astanait.edu.kz>
2024-01-25 23:25:02 +0600

    variables

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,12 @@
 public class Person {
 }
+
+public class Person implements Payable, Comparable<Person> {
+    private String name;
+    private String surname;
+    private final int id;
+    private static int counter = 1;
+
+
+
+}
